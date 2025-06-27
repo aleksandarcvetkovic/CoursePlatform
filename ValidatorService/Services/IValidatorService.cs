192064de82e7534d1e5946df855c6c99ffd6ea3e
Aleksandar Cvetkovic <aleksandar.cvetkovic@vega.local>
2025-06-27 11:28:02 +0200

    Added tests for new service

--- ValidatorService/Services/IValidatorService.cs
+++
@@ -1,9 +0,0 @@
-using FluentValidation.Results;
-using ValidatorService.Models;
-
-namespace ValidatorService.Services;
-
-public interface IValidatorService
-{
-    Task<ValidationResult> ValidateStudentAsync(Student student);
-}
